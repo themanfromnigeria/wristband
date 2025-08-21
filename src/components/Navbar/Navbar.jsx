8dc27a78733bba05a0dd7a6852c51ca522edd736
themanfromnigeria <kindnessalawode@gmail.com>
2025-08-21 22:19:16 +0100

    chore: scroll to section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Navbar.css'
 import logo from '../../assets/wristbands.png'
+import { Link } from 'react-scroll'
 
 const Navbar = () => {
   const [sticky, setSticky] = React.useState(false)
@@ -15,12 +16,12 @@ const Navbar = () => {
     <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
       <img src={logo} alt="" className='logo'/>
       <ul>
-        <li>Home</li>
-        <li>Program</li>
-        <li>About Us</li>
-        <li>Campus</li>
-        <li>Test</li>
-        <li><button className='btn'>Contact Us</button></li>
+        <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
+        <li><Link to='program' smooth={true} offset={-260} duration={500}>Program</Link></li>
+        <li><Link to='about' smooth={true} offset={-150} duration={500}>About Us</Link></li>
+        <li><Link to='campus' smooth={true} offset={-260} duration={500}>Campus</Link></li>
+        <li><Link to='test' smooth={true} offset={-260} duration={500}>Test</Link></li>
+        <li><Link to='contact' smooth={true} offset={-260} duration={500} className='btn'>Contact Us</Link></li>
       </ul>
     </nav>
   )
