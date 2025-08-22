d714d6d0cb385822479fd8a7db8667a41ff30b32
themanfromnigeria <kindnessalawode@gmail.com>
2025-08-22 07:57:36 +0100

    chore: nav bar functionality

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -13,10 +13,16 @@ const Navbar = () => {
     window.addEventListener('scroll', handleScroll)
     return () => window.removeEventListener('scroll', handleScroll)
   }, [])
+
+  const [mobileMenu, setMobileMenu] = React.useState(false)
+  const toggleMenu = () => {
+    mobileMenu ? setMobileMenu(false) : setMobileMenu(true)
+  }
+
   return (
     <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
       <img src={logo} alt="" className='logo'/>
-      <ul>
+      <ul className={mobileMenu ? '' : 'hide-mobile-menu'}>
         <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
         <li><Link to='program' smooth={true} offset={-260} duration={500}>Program</Link></li>
         <li><Link to='about' smooth={true} offset={-150} duration={500}>About Us</Link></li>
@@ -24,7 +30,7 @@ const Navbar = () => {
         <li><Link to='test' smooth={true} offset={-260} duration={500}>Test</Link></li>
         <li><Link to='contact' smooth={true} offset={-260} duration={500} className='btn'>Contact Us</Link></li>
       </ul>
-      <img src={menu} alt="" className='menu-icon'/>
+      <img src={menu} alt="" className='menu-icon' onClick={toggleMenu}/>
     </nav>
   )
 }
